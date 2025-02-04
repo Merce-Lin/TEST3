32a258e6ae3cab79e7ee80dae666f6c3db68bdfc
Merce-Lin <m3rcillicious@gmail.com>
2025-02-04 19:00:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+let char;
+let level;
+let xp = 0;
+let gold = 50;
+let health = 100;
+
+const CreateButton = document.querySelector("#create");
+const LoadButton = document.querySelector("#load");
+const Popup = document.querySelector("#popup");
+
+CreateButton.onclick = function(){
+  Popup.style.display = "block";
+};
+
+LoadButton.onclick = LoadCharacter;
