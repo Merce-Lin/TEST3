83d09541d3716070b6b856d7accbe3c9d2645d98
Merce-Lin <m3rcillicious@gmail.com>
2025-02-05 22:32:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,15 +9,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://Merci:GameOn@gameon.xxqwr.mongodb.net/
-", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+mongoose.connect("mongodb+srv://Merci:GameOn@gameon.xxqwr.mongodb.net/characters?retryWrites=true&w=majority");
+        
 // Define Mongoose Schema
 const CharacterSchema = new mongoose.Schema({
+  _id: { type: String, required: true},
   name: { type: String, required: true },
+  gold: { type: Number, required: true },      
 });
 
 const Character = mongoose.model("Character", CharacterSchema);
@@ -25,10 +23,10 @@ const Character = mongoose.model("Character", CharacterSchema);
 // API Endpoint to Create Character
 app.post("/api/characters", async (req, res) => {
   try {
-    const { name } = req.body;
+    const { _id, name, gold } = req.body;
     if (!name) return res.status(400).json({ message: "Name is required" });
 
-    const newCharacter = new Character({ name });
+    const newCharacter = new Character({ _id, name, gold });
     await newCharacter.save();
 
     res.status(201).json({ message: "Character created successfully", character: newCharacter });
